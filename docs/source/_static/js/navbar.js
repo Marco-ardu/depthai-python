ee76b3e022786e425a0d909c48853531fa4e4665
Marco-ardu <101230776+Marco-ardu@users.noreply.github.com>
2024-05-07 16:22:55 +0800

    Update navbar.js

--- docs/source/_static/js/navbar.js
+++ docs/source/_static/js/navbar.js
@@ -34,7 +34,7 @@ if (!String.prototype.includes) {
     <div class="navbar_menu">
         <div class="img">
             <a href="https://www.oakchina.cn/">
-                <img src="https://www.oakchina.cn/wp-content/uploads/2021/12/cropped-OAKChina-01.png" alt="ArduCam">
+                <img src="https://www.oakchina.cn/wp-content/uploads/2024/05/logo_2024.svg" alt="ArduCam">
             </a>
         </div>
         <div class="menu_list">
